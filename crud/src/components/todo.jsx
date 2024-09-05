4a083cca25f28342020e25a2018477d389defd31
joaopedro <joaopedrofernandess100@gmail.com>
2024-09-05 19:21:02 -0300

    quarto commit

--- crud/src/components/todo.jsx
+++ crud/src/components/todo.jsx
@@ -4,7 +4,7 @@ const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
     <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through"  : ""}}>
       <div className="content">
-      <button className={todo.isCompleted ? "urgencia complete-urgency" : "urgencia"}></button>
+      <button className={todo.isCompleted ? "urgencia complete-urgencia" : "urgencia"}></button>
       
         <p>{todo.text}</p>
         <p>{todo.textarea}</p>
@@ -16,6 +16,7 @@ const Todo = ({ todo, removeTodo, completeTodo }) => {
         <button className="complete"onClick={() => completeTodo(todo.id)}>Completar</button>
         <button className="edit"onClick={("")}>Editar</button>
         <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
+        
         
       </div>
     </div>
